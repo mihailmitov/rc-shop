6d94c6046d8e35e709ec8c481611dfae38b14d73
mihailmitov <mihail.mitov@droxic.com>
2023-10-25 12:45:56 +0300

    feat: add and configure tabs navigator

--- src/screens/navigators/RootNavigator.tsx
+++ src/screens/navigators/RootNavigator.tsx
@@ -1,26 +1,32 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
+import { NavigatorScreenParams } from "@react-navigation/native";
 
-
-import {createNativeStackNavigator} from "@react-navigation/native-stack"
-import HomeScreen from '../HomeScreen';
-import DetailsScreen from '../DetailsScreen';
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "../HomeScreen";
+import DetailsScreen from "../DetailsScreen";
+import TabsNavigator, { TabsStackParamList } from "./TabsNavigator";
 
 export type RootStackParamList = {
-  HomeScreen: undefined;
-  DetailsScreen: undefined;
-}
+  TabsStack: NavigatorScreenParams<TabsStackParamList>;
+  Details: undefined;
+};
 
-const RootStack = createNativeStackNavigator<RootStackParamList>() 
-
+const RootStack = createNativeStackNavigator<RootStackParamList>();
 
 const RootNavigator = () => {
   return (
     <RootStack.Navigator>
-      <RootStack.Screen name="HomeScreen" component={HomeScreen} />
-      <RootStack.Screen name="DetailsScreen" component={DetailsScreen} />
+      <RootStack.Screen
+        name="TabsStack"
+        component={TabsNavigator}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <RootStack.Screen name="Details" component={DetailsScreen} />
     </RootStack.Navigator>
-  )
-}
+  );
+};
 
-export default RootNavigator
+export default RootNavigator;
