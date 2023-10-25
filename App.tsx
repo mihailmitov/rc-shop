e01ecc764f219720e731d539a7b086f3e2eab0d8
mihailmitov <mihail.mitov@droxic.com>
2023-10-25 18:10:04 +0300

    style: add theme

--- App.tsx
+++ App.tsx
@@ -1,12 +1,44 @@
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  Theme,
+  DefaultTheme,
+  DarkTheme,
+} from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, useColorScheme } from "react-native";
 import RootNavigator from "./src/screens/navigators/RootNavigator";
+import { useMemo } from "react";
 
 export default function App() {
+  const colorScheme = useColorScheme();
+
+  const theme: Theme = useMemo(
+    () =>
+      colorScheme === "dark"
+        ? {
+            ...DarkTheme,
+            colors: {
+              ...DarkTheme.colors,
+              primary: "#fff",
+              text: "#fff",
+            },
+          }
+        : {
+            ...DefaultTheme,
+            colors: {
+              ...DefaultTheme.colors,
+              background: "#f5f5f5",
+              text: "#191919",
+              border: "#D9D9D9",
+              primary: "#191919",
+            },
+          },
+    [colorScheme]
+  );
+  
   return (
     <View style={styles.container}>
-      <NavigationContainer>
+      <NavigationContainer theme={theme}>
         <RootNavigator />
         <StatusBar style="dark" />
       </NavigationContainer>
